302e29b69826944ad931123e8dc4b14aa59f715d
Abdulrahman <ethanhunt4d@gmail.com>
2023-04-17 13:46:32 +0200

    Final Edit!

--- Alpha_Channel_Extractor.py
+++ Alpha_Channel_Extractor.py
@@ -2,24 +2,44 @@ import os
 import time
 from PIL import Image
 
-dirPath = input("Enter The Path Please: ")  # Get all files inside this directory
-dirList = os.listdir(dirPath)
-# print(dirList)  # prints all files # Just for Debugging
+def Operation():
+    source_directory = input("Enter source Path Please: ")  # Get all files inside this directory
+    #target_directory = input("Enter targeted Path Please: ")  # Save all files to this directory
 
-x = 0
-z = 0
-for file in os.listdir(dirPath):
-    try:
-        if ("spc" in file) or ("sp" in file):  # why "spc"?... because usually cos is located in the tga images with spc or just sp in it's naming
-            img = Image.open(f"{dirPath}/{file}")
-            r, g, b, a = img.split()
-            a.save(f"{file.split('.')[0]}.tga")
-        x += 1
-    except ValueError:
-        z = 1
-        print(f"Image {file} doesn't have Alpha channel")
+    for filename in os.listdir(source_directory):
+        # Check if file is a TGA image
+        if filename.endswith(".tga"):
+            # Check if filename contains "spc" or "sp"
+            if ("spc" in filename) or ("sp" in filename):
+                # Open the TGA image and extract the alpha channel
+                filepath = os.path.join(source_directory, filename)
+                image = Image.open(filepath)
+                alpha_channel = image.split()[-1]
 
-if z != 1:
-    print("Extraction Completed")
-    time.sleep(2)
+                # Construct new filename with "Ncos" instead of "spc" or "sp",
+                # and remove any instances of "~", "&", or numbers
+                new_filename = filename.replace("spc", "Ncos").replace("sp", "Ncos").replace("~", "").replace("&", "").translate(str.maketrans("", "", "0123456789"))
 
+                # Save the alpha channel as a separate image in the target directory
+                target_file = os.path.join(source_directory, new_filename)
+                alpha_channel.save(target_file)
+
+                # Rename the original file with any numbers, "&", and "~" symbols removed
+                os.rename(os.path.join(source_directory, filename), os.path.join(source_directory, filename.translate(str.maketrans("", "", "0123456789&~"))))
+
+                # Do something with the extracted alpha channel
+                print("Extracted alpha channel from", filename, "and saved it as", target_file)
+            else:
+                # Do something with non-matching files
+                print(filename, "does not contain 'spc' or 'sp'")
+
+    print("\n\n")
+    print("Extracting Finished Successfully")
+    time.sleep(1)
+    again = input("\nDo you want to Extract Again?: y/n")
+    if again == "y":
+        Operation()
+    else:
+        os._exit()
+
+Operation()
